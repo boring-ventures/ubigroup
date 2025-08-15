f4c7e12d9362ad2712d33534f6050edaaccbbf0b
David Ariel Calvi Arce <david.calvi.18@gmail.com>
2025-08-15 16:47:54 -0400

    Update Next.js and TypeScript configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      ["@logos"]: path.resolve(__dirname, "public/assets/logos"),
+    };
+    return config;
+  },
   async headers() {
     return [
       {
